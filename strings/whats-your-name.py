3926a67464abdff432075471233c620a11bbbb66
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-22 00:03:40 +0530

    solves whats ayour name

---
+++ strings/whats-your-name.py
@@ -0,0 +1,7 @@
+def print_full_name(firstName, lastName):
+    print('Hello ' + firstName + ' ' + lastName+ '! You just delved into python.')
+
+
+first = input()
+last = input()
+print_full_name(first, last)
