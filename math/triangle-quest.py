0023c4a0fbe4c77a56c962ecafd59e22e051bcde
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-22 21:26:17 +0530

    solves triangle quest

---
+++ math/triangle-quest.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-quest-1/problem
+
+n = int(input())
+
+for i in range(n):
+    # I learn new things everyday
+    print((10**(i)//9)*i)
+    # print("{}".format(10)*5)
