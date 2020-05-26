8c0f36a5896b52233c778a50fa3206fa565d2318
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-27 00:34:07 +0530

    solves numpy arrays

---
+++ numpy-python/arrays.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/np-arrays/problem
+
+import numpy
+
+
+def arrays(arr):
+    return numpy.array(arr[::-1], float)
+
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
